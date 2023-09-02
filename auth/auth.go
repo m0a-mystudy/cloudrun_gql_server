1b70b59f2f0cf603654f048200fc5759a573ee0f
makoto abe <abe00makoto@gmail.com>
2023-09-02 18:04:43 +0900

    Todo (#7)
    * Generate schema files in nested directory
    
    * Add auth schema and user table migration
    
    * Update database.sqlite3
    
    * gen models
    
    * Add signup resolver with JWT token generation
    
    * Update auth.resolvers.go with login implementation
    
    * Update golang.org/x/crypto to v0.12.0
    
    * Add authentication middleware to server
    
    * Update .gitignore and implement ChangePassword function
    
    * delete
    
    * Update dependencies and rename GraphQL schema and resolvers for todos
    
    * Create migration for todos table
    
    * gen codes
    
    * Remove unused code for UserTableColumns
    
    * add newTodo logic
    
    * Add "github.com/samber/lo" dependency and update queryResolver.Todos to fetch todos and user information
    
    * Toggle todo done feature implementation
    
    * Sort todos by done status and ID

--- auth/auth.go
+++ auth/auth.go
@@ -1,11 +1,14 @@
 package auth
 
 import (
+	"context"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 	"github.com/m0a/cloudrun_gql_server/models"
+	"github.com/volatiletech/null/v8"
 )
 
 func GenerateJWTToken(user *models.User) (string, error) {
@@ -37,4 +40,27 @@ func getGoogleProjectID() (string, error) {
 	// }
 	// return projectID, nil
 	return "test", nil
+}
+
+// GetUserIDFromContext はコンテキストからユーザーIDを取得する関数です。
+func GetUserIDFromContext(ctx context.Context) (null.Int64, error) {
+	tokenString, ok := ctx.Value("token").(string)
+	if !ok || tokenString == "" {
+		return null.Int64{}, fmt.Errorf("unauthenticated")
+	}
+
+	claims := &jwt.StandardClaims{}
+	_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte("my-secret-key"), nil // 秘密鍵を指定
+	})
+	if err != nil {
+		return null.Int64{}, fmt.Errorf("invalid token: %v", err)
+	}
+
+	userID, err := strconv.ParseInt(claims.Subject, 10, 64)
+	if err != nil {
+		return null.Int64{}, fmt.Errorf("invalid user ID: %v", err)
+	}
+
+	return null.Int64From(userID), nil
 }
--- graph/todo.resolvers.go
+++ graph/todo.resolvers.go
@@ -7,18 +7,131 @@ package graph
 import (
 	"context"
 	"fmt"
+	"strconv"
 
+	"github.com/m0a/cloudrun_gql_server/auth"
 	"github.com/m0a/cloudrun_gql_server/graph/model"
+	"github.com/m0a/cloudrun_gql_server/models"
+	"github.com/samber/lo"
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+	// GetUserIDFromContext関数を使用してユーザーIDを取得
+	userID, err := auth.GetUserIDFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// 新しいTodoアイテムをデータベースに挿入
+	newTodo := &models.Todo{
+		Text:   input.Text,
+		Done:   false, // デフォルト値
+		UserID: userID,
+	}
+	err = newTodo.Insert(ctx, r.DB, boil.Infer())
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert new todo: %v", err)
+	}
+
+	u, err := models.Users(models.UserWhere.ID.EQ(userID)).One(ctx, r.DB)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user: %v", err)
+	}
+
+	return &model.Todo{
+		ID:   strconv.FormatInt(newTodo.ID.Int64, 10),
+		Text: newTodo.Text,
+		Done: newTodo.Done,
+		User: &model.User{
+			ID:       strconv.FormatInt(u.ID.Int64, 10),
+			Email:    u.Email,
+			Username: &u.Username.String,
+		},
+	}, nil
+}
+
+// ToggleTodoDone is the resolver for the toggleTodoDone field.
+func (r *mutationResolver) ToggleTodoDone(ctx context.Context, id string) (*model.Todo, error) {
+	userID, err := auth.GetUserIDFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := models.Users(models.UserWhere.ID.EQ(userID)).One(ctx, r.DB)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user: %v", err)
+	}
+
+	// IDからTodoを検索
+	todoID, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("invalid id: %v", err)
+	}
+
+	todo, err := models.FindTodo(ctx, r.DB, null.Int64From(todoID))
+	if err != nil {
+		return nil, fmt.Errorf("Todo not found: %v", err)
+	}
+
+	// doneフィールドをトグル
+	todo.Done = !todo.Done
+
+	// データベースを更新
+	_, err = todo.Update(ctx, r.DB, boil.Infer())
+	if err != nil {
+		return nil, fmt.Errorf("Failed to update Todo: %v", err)
+	}
+
+	return &model.Todo{
+		ID:   id,
+		Text: todo.Text,
+		Done: todo.Done,
+		User: &model.User{
+			ID:       strconv.FormatInt(user.ID.Int64, 10),
+			Email:    user.Email,
+			Username: &user.Username.String,
+		},
+	}, nil
 }
 
 // Todos is the resolver for the todos field.
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+	// GetUserIDFromContext関数を使用してユーザーIDを取得
+	userID, err := auth.GetUserIDFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	u, err := models.Users(models.UserWhere.ID.EQ(userID)).One(ctx, r.DB)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user: %v", err)
+	}
+	boil.DebugMode = true
+	todos, err := models.Todos(
+		models.TodoWhere.UserID.EQ(userID),
+		qm.OrderBy(models.TodoColumns.Done+" DESC"),
+		qm.OrderBy(models.TodoColumns.ID+" ASC"),
+	).
+		All(ctx, r.DB)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get todos: %v", err)
+	}
+	boil.DebugMode = false
+	return lo.Map(todos, func(t *models.Todo, _ int) *model.Todo {
+		return &model.Todo{
+			ID:   strconv.FormatInt(t.ID.Int64, 10),
+			Text: t.Text,
+			Done: t.Done,
+			User: &model.User{
+				ID:       strconv.FormatInt(u.ID.Int64, 10),
+				Email:    u.Email,
+				Username: &u.Username.String,
+			},
+		}
+	}), nil
 }
 
 // Mutation returns MutationResolver implementation.
