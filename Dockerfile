1ca33d8401293c6ec4aae6702d288d703e6d1eaf
makoto abe <abe00makoto@gmail.com>
2023-09-04 22:55:42 +0900

    Update Dockerfile and run.sh for SQLite migration and restoration

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ COPY --from=go  /usr/local/go /usr/local/go
 ENV PATH /usr/local/go/bin:$PATH
 ENV GOBIN /usr/local/go/bin
 
+RUN go install -tags 'sqlite3' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+
 COPY ./ /app
 WORKDIR /app
 RUN go build -o main ./cmd/server
@@ -16,8 +18,8 @@ FROM alpine
 # 1つ目のステージからバイナリだけコピー
 COPY --from=go-build /app/main ./main
 COPY --from=go-build /app/run.sh ./run.sh
-COPY --from=litestream /usr/local/bin/litestream /usr/local/go/bin/litestream
-COPY --from=go-build /app/database.sqlite3 database.sqlite3
+COPY --from=litestream /usr/local/bin/litestream /usr/local/bin/litestream
+COPY --from=go-build /usr/local/go/bin/migrate /usr/local/bin/migrate
 COPY --from=go-build /app/litestream.yml /etc/litestream.yml
 
 CMD ["sh", "run.sh"]
--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-# コンテナ起動時に持っているSQLiteのデータベースファイルは、
-# 後続処理でリストアに成功したら削除したいので、リネームしておく
-if [ -f ./database.sqlite3 ]; then
-  mv ./database.sqlite3 ./database.sqlite3.bk
-fi
 
 # Cloud Storage からリストア
 litestream restore -if-replica-exists -config /etc/litestream.yml ./database.sqlite3
@@ -13,12 +8,11 @@ litestream restore -if-replica-exists -config /etc/litestream.yml ./database.sql
 if [ -f ./database.sqlite3 ]; then
   # リストアに成功したら、リネームしていたファイルを削除
   echo "---- Restored from Cloud Storage ----"
-  rm ./database.sqlite3
 else
   # 初回起動時にはレプリカが未作成であり、リストアに失敗するので、
-  # その場合には、冒頭でリネームしたdbファイルを元の名前に戻す
+  # その場合には migrationを実行する
   echo "---- Failed to restore from Cloud Storage ----"
-  mv ../database.sqlite3.bk ././database.sqlite3
+  migrate -database 'sqlite3://database.sqlite3' -path migrations/ up
 fi
 
 # メインプロセスに、litestreamによるレプリケーション、
