5a5126d0783adc083216c2512005e633a34b5455
makoto abe <abe00makoto@gmail.com>
2023-09-18 15:34:46 +0900

    init frontend

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/app/layout';
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>This is the about page.</p>
+    </Layout>
+  );
+};
+
+export default About;
